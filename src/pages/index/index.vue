a0fa9d5623ccba25dab621d8da1ae59d41c7a34b
fanfanxx <3279913400@qq.com>
2024-07-19 09:55:13 +0800

    首页分类组件的实现

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -4,6 +4,7 @@ import { getHomeBannerAPI } from '@/services/home'
 import { onLoad } from '@dcloudio/uni-app'
 import { ref } from 'vue'
 import type { BannerItem } from '@/types/home'
+import CategoryPanel from './components/CategoryPanel.vue'
 
 const bannerList = ref<BannerItem[]>([])
 const getHomeBannerDataList = async () => {
@@ -18,8 +19,11 @@ onLoad(() => {
 <template>
   <CustomNavbar />
   <ShopSwiper :list="bannerList" />
+  <CategoryPanel />
 </template>
 
 <style lang="scss">
-//
+page {
+  background-color: #f7f7f7;
+}
 </style>
