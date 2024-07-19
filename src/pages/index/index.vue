90bf0cf80a281e9ec2067aec75c0cae3172ba8ce
fanfanxx <3279913400@qq.com>
2024-07-19 11:48:12 +0800

    猜你喜欢模块-scrll-view滑动容器布局封装

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -5,7 +5,7 @@ import { onLoad } from '@dcloudio/uni-app'
 import { ref } from 'vue'
 import type { BannerItem, CategoryItem, HotItem } from '@/types/home'
 import CategoryPanel from './components/CategoryPanel.vue'
-import HotPanelVue from './components/HotPanel.vue'
+import HotPanel from './components/HotPanel.vue'
 
 // 获取轮播图数据
 const bannerList = ref<BannerItem[]>([])
@@ -28,6 +28,8 @@ const getHomeHotData = async () => {
   hotList.value = res.result
   console.log(res)
 }
+
+// 生命周期函数
 onLoad(() => {
   getHomeBannerData()
   getHomeCategoryData()
@@ -36,14 +38,28 @@ onLoad(() => {
 </script>
 
 <template>
+  <!-- 导航栏 -->
   <CustomNavbar />
-  <ShopSwiper :list="bannerList" />
-  <CategoryPanel :list="categoryList" />
-  <HotPanelVue :list="hotList" />
+  <scroll-view class="scroll" scroll-y>
+    <!-- 轮播图 -->
+    <ShopSwiper :list="bannerList" />
+    <!-- 分类模块 -->
+    <CategoryPanel :list="categoryList" />
+    <!-- 热门模块 -->
+    <HotPanel :list="hotList" />
+    <!-- 猜你喜欢模块 -->
+    <ShopGuess />
+  </scroll-view>
 </template>
 
 <style lang="scss">
 page {
   background-color: #f7f7f7;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+}
+.scroll {
+  flex: 1;
 }
 </style>
--- src/types/component.d.ts
+++ src/types/component.d.ts
@@ -1,6 +1,8 @@
 import ShopSwiper from './ShopSwiper.vue'
+import ShopGuess from '@/components/ShopGuess.vue'
 declare module 'vue' {
   export interface GlobalComponents {
     ShopSwiper: typeof ShopSwiper
+    ShopGuess: typeof ShopGuess
   }
 }
